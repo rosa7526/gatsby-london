0781f5d9e03b464882960270e877f8e18e58c877
rosa7526 <70865190+rosa7526@users.noreply.github.com>
2020-09-10 10:07:09 +0900

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
-  name: 'London',
-  shortName: 'London',
+  name: 'Rosa Portfolio',
+  shortName: 'Rosa Portfolio',
   description: 'A bold, minimal theme for Gatsby, focused on clean typography & beautiful imagery',
   url: 'https://gatsby-london.netlify.com', // Domain of your site without prefix!
   prefix: '/',
