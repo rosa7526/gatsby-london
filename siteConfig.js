9146d84069ff22e7b420eda056907b5a46dc53c5
rosa7526 <70865190+rosa7526@users.noreply.github.com>
2020-11-18 14:06:44 +0900

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
   name: 'Rosa Portfolio',
   shortName: 'Rosa Portfolio',
-  description: 'A bold, minimal theme for Gatsby, focused on clean typography & beautiful imagery',
-  url: 'https://gatsby-london.netlify.com', // Domain of your site without prefix!
+  description: 'Portfolio 2020',
+  url: 'https://rosaportfoliodemo-1.netlify.app', // Domain of your site without prefix!
   prefix: '/',
   author: 'Imed Adel',
   twitter: 'Imed_Adel'
